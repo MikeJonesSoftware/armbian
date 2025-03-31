MikeJonesSoftware/armbian

commit 0b3ce2c5ea116cca637ae2e060072271ad9bc233

Fix for ethernet warnings in dmesg output

config/boards/odroidm2.csc
@@ -10,9 +10,9 @@ IMAGE_PARTITION_TABLE="gpt"
 FULL_DESKTOP="no"
 BOOT_LOGO="yes"
 
-BOOTBRANCH_BOARD="tag:v2025.04-rc2"
+BOOTBRANCH_BOARD="tag:v2025.04-rc4"
 BOOTBRANCH="${BOOTBRANCH_BOARD}"
-BOOTPATCHDIR="v2025.04-rc2"
+BOOTPATCHDIR="v2025.04-rc4"
 
 BOOTCONFIG="odroid-m2-rk3588s_defconfig"
 BOOTDIR="u-boot-${BOARD}" # do not share u-boot directory

patch/u-boot/v2025.04-rc4/board_odroidm2/001-odroid-m2-fix-for-ethernet.patch
@@ -1,0 +1,47 @@
+diff --git a/dts/upstream/src/arm64/rockchip/rk3588s-odroid-m2.dts b/dts/upstream/src/arm64/rockchip/rk3588s-odroid-m2.dts
+index 8f034c6d49..ef0f428004 100644
+--- a/dts/upstream/src/arm64/rockchip/rk3588s-odroid-m2.dts
++++ b/dts/upstream/src/arm64/rockchip/rk3588s-odroid-m2.dts
+@@ -229,9 +229,9 @@
+ };
+
+ &gmac1 {
+-	clock_in_out = "output";
++	clock_in_out = "input";
+	phy-handle = <&rgmii_phy1>;
+-	phy-mode = "rgmii-id";
++	phy-mode = "rgmii";
+	phy-supply = <&vcc_3v3_s0>;
+	pinctrl-names = "default";
+	pinctrl-0 = <&gmac1_miim
+@@ -241,6 +241,8 @@
+		     &gmac1_rgmii_bus
+		     &gmac1_clkinout>;
+	status = "okay";
++	tx_delay = <0x3a>;
++	rx_delay = <0x3e>;
+ };
+
+ &gpu {
+@@ -400,6 +402,8 @@
+	rgmii_phy1: ethernet-phy@1 {
+		compatible = "ethernet-phy-id001c.c916";
+		reg = <1>;
++		pinctrl-names = "default";
++		pinctrl-0 = <&rtl8211f_rst>;
+		reset-assert-us = <20000>;
+		reset-deassert-us = <100000>;
+		reset-gpios = <&gpio3 RK_PB7 GPIO_ACTIVE_LOW>;
+@@ -475,6 +479,12 @@
+		};
+	};
+
++	rtl8211f {
++		rtl8211f_rst: rtl8211f-rst {
++			rockchip,pins = <3 RK_PB7 RK_FUNC_GPIO &pcfg_pull_none>;
++		};
++	};
++
+	usb {
+		usb2_host_pwren: usb2-host-pwren {
+			rockchip,pins = <1 RK_PC6 RK_FUNC_GPIO &pcfg_pull_none>;

patch/kernel/archive/rockchip64-6.14/board-odroidm2-fix-for-ethernet.patch
@@ -1,0 +1,47 @@
+diff --git a/arch/arm64/boot/dts/rockchip/rk3588s-odroid-m2.dts b/arch/arm64/boot/dts/rockchip/rk3588s-odroid-m2.dts
+index 8f034c6d494c..ef0f4280048d 100644
+--- a/arch/arm64/boot/dts/rockchip/rk3588s-odroid-m2.dts
++++ b/arch/arm64/boot/dts/rockchip/rk3588s-odroid-m2.dts
+@@ -229,9 +229,9 @@ &cpu_l3 {
+ };
+
+ &gmac1 {
+-	clock_in_out = "output";
++	clock_in_out = "input";
+	phy-handle = <&rgmii_phy1>;
+-	phy-mode = "rgmii-id";
++	phy-mode = "rgmii";
+	phy-supply = <&vcc_3v3_s0>;
+	pinctrl-names = "default";
+	pinctrl-0 = <&gmac1_miim
+@@ -241,6 +241,8 @@ &gmac1_rgmii_clk
+		     &gmac1_rgmii_bus
+		     &gmac1_clkinout>;
+	status = "okay";
++	tx_delay = <0x3a>;
++	rx_delay = <0x3e>;
+ };
+
+ &gpu {
+@@ -400,6 +402,8 @@ &mdio1 {
+	rgmii_phy1: ethernet-phy@1 {
+		compatible = "ethernet-phy-id001c.c916";
+		reg = <1>;
++		pinctrl-names = "default";
++		pinctrl-0 = <&rtl8211f_rst>;
+		reset-assert-us = <20000>;
+		reset-deassert-us = <100000>;
+		reset-gpios = <&gpio3 RK_PB7 GPIO_ACTIVE_LOW>;
+@@ -475,6 +479,12 @@ pcf8563_int: pcf8563-int {
+		};
+	};
+
++	rtl8211f {
++		rtl8211f_rst: rtl8211f-rst {
++			rockchip,pins = <3 RK_PB7 RK_FUNC_GPIO &pcfg_pull_none>;
++		};
++	};
++
+	usb {
+		usb2_host_pwren: usb2-host-pwren {
+			rockchip,pins = <1 RK_PC6 RK_FUNC_GPIO &pcfg_pull_none>;

commit 87dd11871b48a0cd67b0458d7e8f6098e82e2b16

odroidm2: use standard `v2025.04` (drop `-rc4`) from u-boot patch folder name
- Fixes: 0b3ce2c5ea116cca637ae2e060072271ad9bc233 (PR https://github.com/armbian/build/pull/7981)

config/boards/odroidm2.csc
@@ -12,7 +12,7 @@ BOOT_LOGO="yes"
 
 BOOTBRANCH_BOARD="tag:v2025.04-rc4"
 BOOTBRANCH="${BOOTBRANCH_BOARD}"
-BOOTPATCHDIR="v2025.04-rc4"
+BOOTPATCHDIR="v2025.04"
 
 BOOTCONFIG="odroid-m2-rk3588s_defconfig"
 BOOTDIR="u-boot-${BOARD}" # do not share u-boot directory