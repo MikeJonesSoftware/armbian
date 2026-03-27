2a8f9b79b31bb8c0e950f69c58f618f9418b116a
Mike Jones <mike@indianajones.tv>
2026-03-27 20:56:38 +0000

    Add build files for Waveshare Core3566

---
+++ userpatches/extensions/customize-image-waveshare-core3566.sh
@@ -0,0 +1,179 @@
+
+
+function pre_customize_image__set_waveshare_values { 
+	echo "Configuring waveshare-3566-specific values..."
+
+}
+
+function post_customize_image__done_with_customization { 
+	echo "Done with customization!!!"
+}
+
+function post_family_tweaks__preset_configs() {
+	display_alert "$BOARD" "preset configs for rootfs" "info"
+
+	# CPU speed
+	CPUSPEED=816000
+
+	# DNS-SD service discovery can be found at the bottom of this file
+
+	# Allow the user to set the static IP address, mask, and gateway
+	# Default Subnet mask and Gateway are provided if not set by the user
+	# DHCP will be used by default
+	if [ "${STATIC_IP_ADDRESS}" ]; then
+		PRESET_STATIC_IP=1
+	else
+		PRESET_STATIC_IP=0					# Default to DHCP
+		STATIC_IP_ADDRESS='192.168.0.100'	# Placeholder static IP address
+	fi
+	if [ -z "${STATIC_MASK}" ]; then
+		STATIC_MASK='255.255.255.0'
+	fi
+	if [ -z "${STATIC_GATEWAY}" ]; then
+		STATIC_GATEWAY='192.168.0.1'	# Placeholder static gateway
+	fi
+
+	# Fixed answers for the initial bootup configuration questions
+	# Note that you must create the following environment variables before running compile.sh:
+	#    ROOTPW - the new root password
+	#    NEWUSER - the new username
+	#    NEWUSERPW - the new user password
+	cat > "${SDCARD}"/root/.not_logged_in_yet << EOF
+	PRESET_NET_CHANGE_DEFAULTS=1
+	PRESET_NET_ETHERNET_ENABLED=1
+	PRESET_NET_WIFI_ENABLED=0
+	PRESET_NET_WIFI_SSID='MySSID'
+	PRESET_NET_WIFI_KEY='MyWiFiKEY'
+	PRESET_NET_WIFI_COUNTRYCODE='GB'
+	PRESET_NET_USE_STATIC=${PRESET_STATIC_IP}
+	PRESET_NET_STATIC_IP='${STATIC_IP_ADDRESS}'
+	PRESET_NET_STATIC_MASK='${STATIC_MASK}'
+	PRESET_NET_STATIC_GATEWAY='${STATIC_GATEWAY}'
+	PRESET_NET_STATIC_DNS='8.8.8.8 8.8.4.4'
+	PRESET_USER_SHELL=bash
+	PRESET_CONNECT_WIRELESS=n
+	SET_LANG_BASED_ON_LOCATION=y
+	PRESET_LOCALE=en_US.UTF-8
+	PRESET_TIMEZONE=Etc/UTC
+	PRESET_ROOT_PASSWORD=$ROOTPW
+	PRESET_USER_NAME=$NEWUSER
+	PRESET_USER_PASSWORD=$NEWUSERPW
+	PRESET_DEFAULT_REALNAME=Armbian
+EOF
+
+	# /root/provisioning.sh
+	# These commands will run automatically on first boot/login
+	# Install:
+	# 	lm-sensors: reading CPU, etc info
+	# 	sysstat: system metrics
+	# 	nvi: vi editor
+	# 	kbuild: kernel build tools
+	# 	devmem2: register I/O poking utility
+	# 	cpufrequtils: CPU frequency scaling
+	# 	dnssd: mDNS/DNS-SD service discovery
+
+	cat > "${SDCARD}"/root/provisioning.sh << EOF
+	export ETHERNET_INTERFACE=\$(/usr/bin/ls /sys/class/net | /usr/bin/grep -v 'lo' | /usr/bin/head -n 1)
+	echo "Detected ethernet interface: \${ETHERNET_INTERFACE}"
+
+	echo "Installing tcpdump..."
+	apt install tcpdump -y -qq
+	echo "Installing lm-sensors..."
+	apt install lm-sensors -y -qq
+	echo "Installing sysstat..."
+	apt install sysstat -y -qq
+	echo "Installing vi..."
+	apt install nvi -y -qq
+	echo "Installing kbuild..."
+	apt install -yy kbuild -qq
+	echo "Installing devmem2..."
+	apt install -y devmem2 -qq
+
+	echo "Installing cpufrequtils..."
+	apt install cpufrequtils -y -qq
+	cat > /etc/default/cpufrequtils << CPUEOF
+ENABLE=true
+MIN_SPEED=$CPUSPEED
+MAX_SPEED=$CPUSPEED
+GOVERNOR=performance
+CPUEOF
+	/etc/init.d/cpufrequtils restart
+
+	# Create a bash script that requests a refresh on the _ssh._tcp service type.
+	# This is used to prevent the switch from pruning the _ssh._tcp service type
+	cat > /usr/local/bin/mdns-pulse.sh << PULSEEOF
+#!/bin/bash
+# Force a fresh process environment to trigger a real wire-level mDNS probe
+#/bin/sh -c "/usr/bin/avahi-browse -rt _ssh._tcp --terminate" > /dev/null 2>&1
+#/bin/sh -c "/usr/bin/avahi-resolve -n $(hostname).local -4" > /dev/null 2>&1
+
+# Use a timestamp to ensure the TXT record is unique every 90 seconds
+NONCE=$(date +%s)
+
+# Use -c to allow the shell to handle the background process and PID
+/bin/sh -c "avahi-publish -s 'Heartbeat-$(hostname)' _heartbeat._tcp 9999 'v=$NONCE' & sleep 2; kill \$!"
+PULSEEOF
+
+	chmod +x /usr/local/bin/mdns-pulse.sh
+
+	# Create a systemd service that executes the mdns-pulse.sh script
+	cat > /etc/systemd/system/mdns-pulse.service << PULSESERVICEEOF
+[Unit]
+Description=Periodic mDNS Pulse to prevent Switch Pruning
+After=network.target avahi-daemon.service
+
+[Service]
+Type=oneshot
+ExecStart=/usr/local/bin/mdns-pulse.sh
+#CPUSchedulingPolicy=fifo
+#CPUSchedulingPriority=99
+
+[Install]
+WantedBy=multi-user.target
+PULSESERVICEEOF
+
+	# Create a systemd timer that executes the mdns-pulse.service every 90 seconds
+	cat > /etc/systemd/system/mdns-pulse.timer << PULSETIMEREOF
+[Unit]
+Description=Run mDNS Pulse every 90 seconds
+
+[Timer]
+OnBootSec=1min
+OnUnitActiveSec=90s
+AccuracySec=1s
+
+[Install]
+WantedBy=timers.target
+PULSETIMEREOF
+
+	systemctl daemon-reload
+	systemctl enable --now mdns-pulse.timer
+
+	# Update the avahi-daemon.conf file
+	sed -i "
+		s/^#\?host-name=.*/host-name=${BOARD}/
+		s/^#\?domain-name=.*/domain-name=local/
+		s/^#\?allow-interfaces=.*/allow-interfaces=\${ETHERNET_INTERFACE}/
+	" /etc/avahi/avahi-daemon.conf
+
+	cat > /etc/avahi/services/device.service << AVAHIEOF
+<?xml version="1.0" standalone='no'?>
+<!DOCTYPE service-group SYSTEM "avahi-service.dtd">
+<service-group>
+	<name replace-wildcards="yes">%h</name>
+	<service>
+		<type>_ssh._tcp</type>
+		<port>25000</port>
+		<txt-record>nickname=Initialized Waveshare Core3566 Device</txt-record>
+	</service>
+</service-group>
+AVAHIEOF
+	systemctl restart avahi-daemon.service
+
+
+	sed -i '/bin/s//bin:\/home\/$NEWUSER\/bin/' /etc/environment
+		echo "$NEWUSER ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/00-$NEWUSER
+		echo 'Defaults        secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:/home/$NEWUSER/bin"' >> /etc/sudoers.d/00-$NEWUSER
+EOF
+
+}
